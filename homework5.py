d87024e81d16e74e1d6c002310d0c73133e01ffe
AbsoIuteZer00 <163422798+AbsoIuteZer00@users.noreply.github.com>
2024-03-20 12:15:48 +0300

    Add files via upload

---
+++ homework5.py
@@ -0,0 +1,13 @@
+my_list = ["apple", "orange", "grapes", "banana", "peach"]
+print("List: ", my_list)
+print("First element:", my_list[0])
+print("Last element:", my_list[-1])
+print("Sublist:", my_list[2:5])
+my_list[2] = "kiwi"
+print("Modified list:", my_list)
+my_dict = {"apple": "яблоко", "orange": "fruit", "kiwi": "green", "banana": "банан", "peach": "персик"}
+print("Dictionary:", my_dict)
+print("Translation:", my_dict["banana"])
+my_dict["orange"] = "апельсин"
+my_dict["melon"] = "дыня"
+print("Modified dictionary:", my_dict)
