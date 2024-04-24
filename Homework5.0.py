a50bf453905876e3673d3dfd33a6dcb4ccc8bdeb
AbsoIuteZer00 <163422798+AbsoIuteZer00@users.noreply.github.com>
2024-04-24 08:23:24 +0300

    Add files via upload

---
+++ Homework5.0.py
@@ -0,0 +1,11 @@
+class House:
+    """Пример класса"""
+    def __init__(self):
+        self.numberOfFloors = 10
+
+
+house = House()
+for i in range(house.numberOfFloors):
+    """цикл для перебора этажей"""
+    i += 1
+    print(f'Я нахожусь на {i} этаже')
