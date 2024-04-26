e1acdc1a1a1a1c19ee50e8f852f72454eb4d924a
AbsoIuteZer00 <163422798+AbsoIuteZer00@users.noreply.github.com>
2024-04-26 13:07:22 +0300

    Update Homework5.2.py

--- Homework5.2.py
+++ Homework5.2.py
@@ -1,13 +1,15 @@
 class Building:
     """Здание"""
     def __init__(self):
-        self.numberOfFloors = int
-        self.buildingType = str
+        self.numberOfFloors = 0
+        self.buildingType = ''
 
     def __eq__(self, other):
         """Сравнение объектов"""
-        return self.numberOfFloors == self.buildingType
+        return self.numberOfFloors == other.numberOfFloors and self.buildingType == other.buildingType
 
 
-building = Building()
-building.__eq__(1)
+building_ = Building()
+building_2 = Building()
+if Building.__eq__(building_, building_2):
+    print('Здания идентичны')
